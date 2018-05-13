c1eb547a3ac81bcb3f139acc651531bc06d1f0ca
bfchengnuo <bfchengnuo@gmail.com>
2018-05-13 11:50:54 +0800

    更新导航

--- js/data.js
+++ js/data.js
@@ -4,51 +4,51 @@ var FrontGuide = {
         "pageC1": [{
             "pageT2": "Java",
             "pageC2": [{
-                    'Name': 'JavaSE 10 API',
-                    'Title': 'Oracle 官方在线，英文版',
-                    'Href': 'https://docs.oracle.com/javase/10/docs/api/overview-summary.html',
-                    "New": "1"
-                }, {
-                    'Name': 'JavaSE 8 API',
-                    'Title': 'Java 8 版本是目前比较成熟的一个正式版本，9 不做考虑，Oracle 官方在线，英文版',
-                    'Href': 'https://docs.oracle.com/javase/8/docs/api/'
-                }, {
-                    'Name': 'JavaSE 6 中文 API',
-                    'Title': '官方目前唯一的一版中文 API，虽然是 6 版本，在线工具提供',
-                    'Href': 'http://tool.oschina.net/apidocs/apidoc?api=jdk-zh'
-                }
+                'Name': 'JavaSE 10 API',
+                'Title': 'Oracle 官方在线，英文版',
+                'Href': 'https://docs.oracle.com/javase/10/docs/api/overview-summary.html',
+                "New": "1"
+            }, {
+                'Name': 'JavaSE 8 API',
+                'Title': 'Java 8 版本是目前比较成熟的一个正式版本，9 不做考虑，Oracle 官方在线，英文版',
+                'Href': 'https://docs.oracle.com/javase/8/docs/api/'
+            }, {
+                'Name': 'JavaSE 6 中文 API',
+                'Title': '官方目前唯一的一版中文 API，虽然是 6 版本，在线工具提供',
+                'Href': 'http://tool.oschina.net/apidocs/apidoc?api=jdk-zh'
+            }
             ]
         }, {
             "pageT2": "Spring 系列",
             "pageC2": [{
-                    'Name': 'Spring Framework 中文文档（5.0.0.M4）',
-                    'Title': '由 muyinchen 等翻译,完成度挺高的',
-                    'Href': 'https://muyinchen.gitbooks.io/spring-framework-5-0-0-m3/content/'
-                }, {
-                    'Name': 'Spring Framework 5 中文文档（备用）',
-                    'Title': '备用Spring5 中文',
-                    'Href': 'https://lfvepclr.gitbooks.io/spring-framework-5-doc-cn/content/'
-                }, {
-                    'Name': 'Spring MVC 4.2.4 中文文档',
-                    'Title': 'SpringMVC 中文',
-                    'Href': 'http://7xvpsh.com1.z0.glb.clouddn.com/'
-                }, {
-                    'Name': 'Spring Framework 官方文档',
-                    'Title': 'Spring Framework 官方英文文档，包含 Spring MVC',
-                    'Href': 'https://docs.spring.io/spring/docs/current/spring-framework-reference/'
-                }, {
-                    'Name': 'Spring Cloud 中文文档',
-                    'Title': '由 Spring Cloud 中文网提供，版本：Dalston.RELEASE',
-                    'Href': 'https://springcloud.cc/spring-cloud-dalston.html'
-                }, {
-                    'Name': 'Spring Boot 官方文档',
-                    'Title': '官方英文原版，当前版本 1.5.9.RELEASE',
-                    'Href': 'https://docs.spring.io/spring-boot/docs/current/reference/html/'
-                }, {
-                    'Name': 'Spring Boot 中文文档',
-                    'Title': '由网友们翻译，基于 1.5.2.RELEASE',
-                    'Href': 'http://cwiki.apachecn.org/pages/viewpage.action?pageId=10027628'
-                }
+                'Name': 'Spring Framework 中文文档（5.0.0.M4）',
+                'Title': '由 muyinchen 等翻译,完成度挺高的',
+                'Href': 'https://muyinchen.gitbooks.io/spring-framework-5-0-0-m3/content/'
+            }, {
+                'Name': 'Spring Framework 5 中文文档（备用）',
+                'Title': '备用Spring5 中文',
+                'Href': 'https://lfvepclr.gitbooks.io/spring-framework-5-doc-cn/content/'
+            }, {
+                'Name': 'Spring MVC 4.2.4 中文文档',
+                'Title': 'SpringMVC 中文',
+                'Href': 'http://7xvpsh.com1.z0.glb.clouddn.com/'
+            }, {
+                'Name': 'Spring Framework 官方文档',
+                'Title': 'Spring Framework 官方英文文档，包含 Spring MVC',
+                'Href': 'https://docs.spring.io/spring/docs/current/spring-framework-reference/'
+            }, {
+                'Name': 'Spring Cloud 中文文档',
+                'Title': '由 Spring Cloud 中文网提供，版本：Dalston.RELEASE',
+                'Href': 'https://springcloud.cc/spring-cloud-dalston.html'
+            }, {
+                'Name': 'Spring Boot 官方文档',
+                'Title': '官方英文原版，当前版本 1.5.9.RELEASE',
+                'Href': 'https://docs.spring.io/spring-boot/docs/current/reference/html/'
+            }, {
+                'Name': 'Spring Boot 中文文档',
+                'Title': '由网友们翻译，基于 1.5.2.RELEASE',
+                'Href': 'http://cwiki.apachecn.org/pages/viewpage.action?pageId=10027628'
+            }
             ]
         }, {
             "pageT2": "其他框架",
@@ -95,23 +95,30 @@ var FrontGuide = {
         }, {
             "pageT2": "前端常用",
             "pageC2": [{
-                    'Name': 'jQuery 中文API',
-                    'Title': '印记中文出品',
-                    'Href': 'https://www.jquery123.com/'
-                }, {
-                    'Name': 'jQuery 官方API',
-                    'Title': '官方英文 API 文档，包含 jQuery UI API、jQuery Mobile API 入口',
-                    'Href': 'http://api.jquery.com/'
-                }, {
-                    'Name': 'Bootstrap 中文参考手册',
-                    'Title': '社区翻译质量挺高',
-                    'Href': 'https://v3.bootcss.com/getting-started/'
-                }, {
-                    'Name': 'Bootstrap 官方文档',
-                    'Title': '英文原版',
-                    'Href': 'https://getbootstrap.com/docs/4.0/getting-started/introduction/'
-                }
-            ]
+                'Name': 'jQuery 中文API',
+                'Title': '印记中文出品',
+                'Href': 'https://www.jquery123.com/'
+            }, {
+                'Name': 'jQuery 官方API',
+                'Title': '官方英文 API 文档，包含 jQuery UI API、jQuery Mobile API 入口',
+                'Href': 'http://api.jquery.com/'
+            }, {
+                'Name': 'Bootstrap 中文参考手册',
+                'Title': '社区翻译质量挺高',
+                'Href': 'https://v3.bootcss.com/getting-started/'
+            }, {
+                'Name': 'Bootstrap 官方文档',
+                'Title': '英文原版',
+                'Href': 'https://getbootstrap.com/docs/4.0/getting-started/introduction/'
+            },{
+                'Name': 'React 中文文档',
+                'Title': '翻译质量不错',
+                'Href': 'https://doc.react-china.org/'
+            },{
+                'Name': 'webpack中文文档',
+                'Title': '安装官网风格，质量较高',
+                'Href': 'https://webpack.docschina.org'
+            }]
         }, {
             "pageT2": "其他/翻译社区",
             "pageC2": [{
@@ -125,67 +132,67 @@ var FrontGuide = {
         "pageC1": [{
             "pageT2": "编码/解码/加密",
             "pageC2": [{
-                    'Name': 'URL 编码解码',
-                    'Title': '站长工具提供，附带 Unicode、UTF-8 编码，以及时间戳工具',
-                    'Href': 'http://www.templatesy.com/'
-                }, {
-                    'Name': 'Base64 编码解码（支持图片）',
-                    'Title': '支持图片与 Base64 的转换',
-                    'Href': 'http://tool.oschina.net/encrypt?type=3'
-                }, {
-                    'Name': 'MD5 处理',
-                    'Title': '使用频率高，单独拎出来',
-                    'Href': 'http://tool.chinaz.com/Tools/md5.aspx'
-                }, {
-                    'Name': '常用加密解密',
-                    'Title': '除了 AES/DES/RC4/Rabbit/TripleDes 还支持散列和哈希(比如 SHA1/SHA256/MD5)，采用 Crypto-JS 实现',
-                    'Href': 'http://tool.oschina.net/encrypt'
-                }, {
-                    'Name': 'SSL 工具箱',
-                    'Title': '专业 SSL 相关工具，很全',
-                    'Href': 'http://www.ssleye.com/',
-                    "New": "1"
-                }
+                'Name': 'URL 编码解码',
+                'Title': '站长工具提供，附带 Unicode、UTF-8 编码，以及时间戳工具',
+                'Href': 'http://tool.chinaz.com/Tools/urlencode.aspx'
+            }, {
+                'Name': 'Base64 编码解码（支持图片）',
+                'Title': '支持图片与 Base64 的转换',
+                'Href': 'http://tool.oschina.net/encrypt?type=3'
+            }, {
+                'Name': 'MD5 处理',
+                'Title': '使用频率高，单独拎出来',
+                'Href': 'http://tool.chinaz.com/Tools/md5.aspx'
+            }, {
+                'Name': '常用加密解密',
+                'Title': '除了 AES/DES/RC4/Rabbit/TripleDes 还支持散列和哈希(比如 SHA1/SHA256/MD5)，采用 Crypto-JS 实现',
+                'Href': 'http://tool.oschina.net/encrypt'
+            }, {
+                'Name': 'SSL 工具箱',
+                'Title': '专业 SSL 相关工具，很全',
+                'Href': 'http://www.ssleye.com/',
+                "New": "1"
+            }
             ]
         }, {
             "pageT2": "网站检测",
             "pageC2": [{
-                    'Name': 'netcraft 网站体检',
-                    'Title': '主要是检查网站安全性等问题的，信息还算是比较全的',
-                    'Href': 'https://toolbar.netcraft.com/site_report?url='
-                }, {
-                    'Name': 'w3techs 网站技术探测',
-                    'Title': '深入调查网站的信息，基本信息来说还是挺全的',
-                    'Href': 'https://w3techs.com/sites'
-                }, {
-                    'Name': 'BuiltWith 探测网站',
-                    'Title': '信息少些，主要是在服务器、邮件和 ISP 探测上',
-                    'Href': 'https://builtwith.com/'
-                }, {
-                    'Name': 'IP高精定位一',
-                    'Title': '地图显示',
-                    'Href': 'https://www.opengps.cn/Data/IP/LocHighAcc.aspx'
-                }, {
-                    'Name': 'IP高精定位二',
-                    'Title': '相对精确',
-                    'Href': 'http://www.hao7188.com/'
-                }, {
-                    'Name': '多Ping工具一',
-                    'Title': '针对国内网络',
-                    'Href': 'http://ping.chinaz.com/'
-                }, {
-                    'Name': '多Ping工具二',
-                    'Title': '国际知名',
-                    'Href': 'http://ping.pe/'
-                }, {
-                    'Name': '17CE',
-                    'Title': '工具合集，挺强大的',
-                    'Href': 'https://www.17ce.com/'
-                }, {
-                    'Name': 'DNS 查询',
-                    'Title': '可查国内外，被 DNS 污染了么？嘿',
-                    'Href': 'https://www.ipip.net/dns.php'
-                }
+                'Name': 'netcraft 网站体检',
+                'Title': '主要是检查网站安全性等问题的，信息还算是比较全的',
+                'Href': 'https://toolbar.netcraft.com/site_report?url='
+            }, {
+                'Name': 'w3techs 网站技术探测',
+                'Title': '深入调查网站的信息，基本信息来说还是挺全的',
+                'Href': 'https://w3techs.com/sites'
+            }, {
+                'Name': 'BuiltWith 探测网站',
+                'Title': '信息少些，主要是在服务器、邮件和 ISP 探测上',
+                'Href': 'https://builtwith.com/'
+            }, {
+                'Name': 'IP高精定位一',
+                'Title': '地图显示',
+                'Href': 'https://www.opengps.cn/Data/IP/LocHighAcc.aspx'
+            }, {
+                'Name': 'IP高精定位二',
+                'Title': '相对精确',
+                'Href': 'http://www.hao7188.com/'
+            }, {
+                'Name': '多Ping工具一',
+                'Title': '针对国内网络',
+                'Href': 'http://ping.chinaz.com/'
+            }, {
+                'Name': '多Ping工具二',
+                'Title': '国际知名',
+                'Href': 'http://ping.pe/'
+            }, {
+                'Name': '17CE',
+                'Title': '工具合集，挺强大的',
+                'Href': 'https://www.17ce.com/'
+            }, {
+                'Name': 'DNS 查询',
+                'Title': '可查国内外，被 DNS 污染了么？嘿',
+                'Href': 'https://www.ipip.net/dns.php'
+            }
             ]
         }, {
             "pageT2": "便民工具",
@@ -205,6 +212,18 @@ var FrontGuide = {
                 'Name': '无限图床',
                 'Title': '稳定性未知',
                 'Href': 'https://img9.top/'
+            },{
+                'Name': '无限图床loli',
+                'Title': '稳定性未知',
+                'Href': 'https://i.loli.net/'
+            },{
+                'Name': 'waifu2x',
+                'Title': '提高二刺螈图片质量',
+                'Href': 'http://waifu2x.udp.jp/index.zh-CN.html'
+            },{
+                'Name': 'Multi-service',
+                'Title': '二刺螈图片搜索',
+                'Href': 'https://iqdb.org/'
             }]
         }, {
             "pageT2": "安全检测",
@@ -212,41 +231,41 @@ var FrontGuide = {
                 'Name': '360显微镜',
                 'Title': '检测 APP 安全',
                 'Href': 'http://appscan.360.cn/'
-            },{
+            }, {
                 'Name': '哈勃',
                 'Title': '文件分析',
                 'Href': 'https://habo.qq.com/'
             }]
-        },{
+        }, {
             "pageT2": "其他工具",
             "pageC2": [{
                 'Name': 'Json/sql 转 Java/C# 实体 &amp; properties转yam',
                 'Title': '用 IDEA 的同学可以尝试下用 GsonFormat 这个插件，简单便捷！',
                 'Href': 'https://www.bejson.com/json2javapojo/new/'
-            },{
+            }, {
                 'Name': '好玩的 HTML 预览器',
                 'Title': '在线编辑 HTML 代码，高亮 + 特技',
                 'Href': 'https://kushagragour.in/code-blast-codemirror/demo/index.html'
-            },{
+            }, {
                 'Name': 'Shell 命令解析',
                 'Title': '复杂的 shell 看不懂？',
                 'Href': 'https://explainshell.com/'
-            },{
+            }, {
                 'Name': 'StackEdit MD 在线编辑器',
                 'Title': 'markdown 在线编辑',
                 'Href': 'https://stackedit.io/editor'
-            },{
+            }, {
                 'Name': 'jsFuck 编码',
                 'Title': '诡异的 JS ，诡异的结果',
                 'Href': 'http://www.jsfuck.com/'
             }]
-        },{
+        }, {
             "pageT2": "工具整合",
             "pageC2": [{
                 'Name': '在线工具',
                 'Title': '号称程序员的工具箱，种类挺丰富的，看到有代码着色',
                 'Href': 'https://tool.lu/'
-            },{
+            }, {
                 'Name': '菜鸟工具',
                 'Title': '各种在线编译工具，代码压缩工具，重点在前端吧',
                 'Href': 'https://c.runoob.com/'
@@ -255,6 +274,25 @@ var FrontGuide = {
     }, {
         "pageT1": "数据平台",
         "pageC1": [{
+            "pageT2": "开发库",
+            "pageC2": [{
+                'Name': 'Maven 仓库',
+                'Title': '找得到绝大多数的依赖吧',
+                'Href': 'https://mvnrepository.com/'
+            }, {
+                'Name': 'SourceForge',
+                'Title': '曾经的 “Github”，嘿嘿',
+                'Href': 'https://sourceforge.net'
+            }, {
+                'Name': 'Apache Commons',
+                'Title': '专注 java',
+                'Href': 'https://commons.apache.org/'
+            },{
+                'Name': 'Trending repositories on GitHub',
+                'Title': 'Github 排行榜',
+                'Href': 'https://github.com/trending'
+            }]
+        }, {
             "pageT2": "社工库",
             "pageC2": [{
                 'Name': '社工库源码大全',
@@ -315,19 +353,19 @@ var FrontGuide = {
                 'Name': '在线邮件头解析',
                 'Title': 'Email Header Analyzer',
                 'Href': 'http://mxtoolbox.com/EmailHeaders.aspx'
-            },{
+            }, {
                 'Name': 'Ipleak',
                 'Title': '工具集合',
                 'Href': 'https://ipleak.net/'
-            },{
+            }, {
                 'Name': '邮件检测',
                 'Title': '配合头解析',
                 'Href': 'http://tool.chacuo.net/mailverify'
-            },{
+            }, {
                 'Name': 'The Web of WebScan',
                 'Title': '如名字一般',
                 'Href': 'http://www.webscan.cc/'
-            },{
+            }, {
                 'Name': '常见漏洞扫描器测试平台',
                 'Title': '工具集合',
                 'Href': 'http://demo.aisec.cn/demo/'
@@ -338,14 +376,18 @@ var FrontGuide = {
                 'Name': '常见漏洞扫描器测试平台',
                 'Title': '看看就好',
                 'Href': 'http://demo.aisec.cn/demo/'
-            },{
+            }, {
                 'Name': '微信搜索',
                 'Title': '搜狗搜索',
                 'Href': 'http://weixin.sogou.com/'
-            },{
+            }, {
                 'Name': '百度云搜索',
                 'Title': '或者使用 Google site 搜索也行',
                 'Href': 'http://wowenda.com/'
+            }, {
+                'Name': '云盘精灵',
+                'Title': '百度云提取码“破解”？',
+                'Href': 'https://www.yunpanjingling.com/'
             }]
         }]
     }, {
@@ -377,6 +419,44 @@ var FrontGuide = {
                 'Name': '牛客网',
                 'Title': '免试笔试编程检测',
                 'Href': 'https://www.nowcoder.com/'
+            }]
+        }, {
+            "pageT2": "社区",
+            "pageC2": [{
+                'Name': 'IBM developerWorks',
+                'Title': '文章质量很高，中文社区',
+                'Href': 'https://www.ibm.com/developerworks/cn/'
+            },{
+                'Name': 'Stack Overflow',
+                'Title': '没有你找不到的问题',
+                'Href': 'http://stackoverflow.com/'
+            },{
+                'Name': 'SegmentFault',
+                'Title': '中文版的 Stack Overflow',
+                'Href': 'https://segmentfault.com/'
+            },{
+                'Name': 'infoQ',
+                'Title': '高质量分享社区',
+                'Href': 'http://www.infoq.com/cn/'
+            },{
+                'Name': 'Readhub',
+                'Title': '了解最新的IT资讯',
+                'Href': 'https://readhub.me/'
+            },{
+                'Name': '开发者头条',
+                'Title': '了解最新的IT资讯',
+                'Href': 'https://toutiao.io/'
+            },{
+                'Name': '伯乐在线-资讯',
+                'Title': '了解最新的IT资讯',
+                'Href': 'http://top.jobbole.com/'
+            }]
+        },{
+            "pageT2": "其他",
+            "pageC2": [{
+                'Name': '知识树',
+                'Title': '技能树整理',
+                'Href': 'http://treenpool.com/#/home'
             }]
         }]
     }]
